f38dd3dedb0dc6af40bd00c41c70a74f26238db6
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-05-08 00:59:04 +0200

    update db

--- src/services/chars/index.js
+++ src/services/chars/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { db } from "../../server.js"
-
+import stringSimilarity from "string-similarity"
 export const charRouter = Router()
 
 charRouter.get("/all", async (req, res, next) => {
@@ -15,8 +15,10 @@ charRouter.get("/all", async (req, res, next) => {
 
 charRouter.get("/:id", async (req, res, next) => {
   try {
-    const data = await db.data.characters.find(char => char.id === req.params.id)
-    if(data) res.send(data) 
+    const data = await db.data.characters.find(
+      (char) => char.id === req.params.id
+    )
+    if (data) res.send(data)
     else res.sendStatus(404)
   } catch (error) {
     next(error)
@@ -24,30 +26,41 @@ charRouter.get("/:id", async (req, res, next) => {
   }
 })
 
-
-charRouter.put("/connect", async(req,res,next) => {
+charRouter.put("/connect", async (req, res, next) => {
   try {
     const chars = await db.data.characters
     const quotes = await db.data.quotes
 
-    const quotesWithIds = quotes.map(quote => {
-      Object.keys(quote).forEach(key => {
-        Object.keys(quote[key]).forEach(line => {
-          console.log(line);
-          if(line === "character") {
+    const quotesWithIds = quotes.map((quote) => {
+      Object.keys(quote).forEach((key) => {
+        Object.keys(quote[key]).forEach((line) => {
+          if (line === "character") {
             const name = quote[key][line]
-            console.log(chars[0]);
-            quote[key][line] = {
-              name,
-              id: chars.find(pg => pg.name.toLowerCase() === name.toLowerCase())
+            const allchars = chars
+              .filter((pg) =>
+                pg.name.toLowerCase().includes(name.toLowerCase())
+              )
+              .map((pg) => pg.name)
+            if (allchars.length > 0) {
+              let { bestMatch: found } = stringSimilarity.findBestMatch(
+                name.toLowerCase(),
+                allchars
+              )
+              quote[key][line] = {
+                name,
+                id: chars.find(
+                  (pg) => pg.name.toLowerCase() === found.target.toLowerCase()
+                ).id,
+              }
             }
           }
         })
       })
       return quote
     })
+    res.send(quotesWithIds)
   } catch (error) {
     next(error)
-    console.log(error);
+    console.log(error)
   }
 })
