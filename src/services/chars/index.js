8d45571c9cd1812d52f17befee2fce0acd99cd20
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-05-08 01:42:22 +0200

    ok

--- src/services/chars/index.js
+++ src/services/chars/index.js
@@ -26,52 +26,52 @@ charRouter.get("/:id", async (req, res, next) => {
   }
 })
 
-charRouter.put("/connect", async (req, res, next) => {
-  try {
-    const chars = await db.data.characters
-    const quotes = await db.data.quotes
+// charRouter.put("/connect", async (req, res, next) => {
+//   try {
+//     const chars = await db.data.characters
+//     const quotes = await db.data.quotes
 
-    const quotesWithIds = quotes.map((quote) => {
-      Object.keys(quote).forEach((key) => {
-        Object.keys(quote[key]).forEach((line) => {
-          if (line === "character") {
-            let name = quote[key][line].name || quote[key][line]
-            if (quote[key][line].name != "") {
-              console.log(name.split("(")[0] + (name.split(")")[1] || ""))
-              const allchars = chars
-                .filter((pg) =>
-                  pg.name
-                    .toLowerCase()
-                    .includes(
-                      (name.split("(")[0] + (name.split(")")[1] || "")).toLowerCase()
-                    )
-                )
-                .map((pg) => pg.name.split("(")[0] + (pg.name.split(")")[1] || ""))
-              if (allchars.length > 0) {
-                let { bestMatch: found } = stringSimilarity.findBestMatch(
-                  name.split("(")[0] + (name.split(")")[1] || "").toLowerCase(),
-                  allchars
-                )
-                console.log(found);
-                quote[key][line] = {
-                  name,
-                  id: chars.find(
-                    (pg) => pg.name.toLowerCase() === found.target.trim().toLowerCase()
-                  ).id,
-                }
-              }
-            }
-          }
-        })
-      })
-      return quote
-    })
+//     const quotesWithIds = quotes.map((quote) => {
+//       Object.keys(quote).forEach((key) => {
+//         Object.keys(quote[key]).forEach((line) => {
+//           if (line === "character") {
+//             let name = quote[key][line].name || quote[key][line]
+//             if (quote[key][line].name != "") {
+//               console.log(name.split("(")[0] + (name.split(")")[1] || ""))
+//               const allchars = chars
+//                 .filter((pg) =>
+//                   pg.name
+//                     .toLowerCase()
+//                     .includes(
+//                       (name.split("(")[0] + (name.split(")")[1] || "")).toLowerCase()
+//                     )
+//                 )
+//                 .map((pg) => pg.name.split("(")[0] + (pg.name.split(")")[1] || ""))
+//               if (allchars.length > 0) {
+//                 let { bestMatch: found } = stringSimilarity.findBestMatch(
+//                   name.split("(")[0] + (name.split(")")[1] || "").toLowerCase(),
+//                   allchars
+//                 )
+//                 console.log(found);
+//                 quote[key][line] = {
+//                   name,
+//                   id: chars.find(
+//                     (pg) => pg.name.toLowerCase() === found.target.trim().toLowerCase()
+//                   ).id,
+//                 }
+//               }
+//             }
+//           }
+//         })
+//       })
+//       return quote
+//     })
 
-    db.data.quotes = quotesWithIds
-    db.write()
-    res.send(200)
-  } catch (error) {
-    next(error)
-    console.log(error)
-  }
-})
+//     db.data.quotes = quotesWithIds
+//     db.write()
+//     res.send(200)
+//   } catch (error) {
+//     next(error)
+//     console.log(error)
+//   }
+// })
