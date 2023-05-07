fe57d966697c7c794a4084a74d716a4a872fae0c
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-05-07 22:52:15 +0200

    update

--- src/server.js
+++ src/server.js
@@ -23,7 +23,6 @@ export const app = express()
 app.use(cors())
 app.use("/quotes", quotesRouter)
 app.use("/characters", charRouter)
-console.log("ok")
 
 db.read().then(() => {
   app.listen(process.env.PORT, () => {
