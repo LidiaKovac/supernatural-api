1238d77e0199f63f2506660ece1ed62db4ec70c2
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-05-09 18:37:45 +0200

    add correct struct to quotes

--- src/services/quotes/index.js
+++ src/services/quotes/index.js
@@ -1,12 +1,27 @@
 import { Router } from "express"
 import { db } from "../../server.js"
+import { paginate } from "../../utils/index.js"
 
 export const quotesRouter = Router()
 
 quotesRouter.get("/all", async (req, res, next) => {
   try {
     const data = await db.data.quotes
-    res.send(data)
+    let result = []
+    result = paginate(data, req.query.page || 1, req.query.size || 5)
+
+    res.send({
+      data: result,
+      next:
+        "https://supernatural-quotes-api.cyclic.app/quotes/all?page=" +
+        (isNaN((Number(req.query.page) + 1)) ? 2 : (Number(req.query.page) + 1)),
+      prev:
+        Number(req.query.page) > 1
+          ? "https://supernatural-quotes-api.cyclic.app/quotes/all?page=" +
+          (Number(req.query.page) - 1)
+          : null,
+      count: data.length,
+    })
   } catch (error) {
     next(error)
     console.error(error)
