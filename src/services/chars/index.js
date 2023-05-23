d04485a01c1b3b5f593899a5bc710b7fa7682fb3
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-05-23 23:57:14 +0200

    add

--- src/services/chars/index.js
+++ src/services/chars/index.js
@@ -13,6 +13,20 @@ charRouter.get("/all", async (req, res, next) => {
   }
 })
 
+charRouter.put("/clean", async(req,res,next) => {
+  try {
+    const newA = []
+    for (const c of db.data.characters) {
+      if(!newA.map(e => e.id).includes(c.id)) {
+        newA.push(c)
+      }
+    }
+  } catch (error) {
+    
+  }
+})
+
+
 charRouter.get("/:id", async (req, res, next) => {
   try {
     const data = await db.data.characters.find(char => char.id === req.params.id)
@@ -23,3 +37,5 @@ charRouter.get("/:id", async (req, res, next) => {
     console.error(error)
   }
 })
+
+
