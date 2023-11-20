193256d96b4e937a40ce2ab3c0a40d01399e65d0
Lidia Kovac <70687866+LidiaKovac@users.noreply.github.com>
2023-11-20 15:41:52 +0100

    cleanup

--- src/services/chars/index.js
+++ src/services/chars/index.js
@@ -1,8 +1,6 @@
 import { Router } from "express"
 import { db } from "../../server.js"
-import stringSimilarity from "string-similarity"
 import { paginate } from "../../utils/index.js"
-import DomParser from "dom-parser"
 export const charRouter = Router()
 
 charRouter.get("/", async (req, res, next) => {
@@ -49,54 +47,7 @@ charRouter.get("/", async (req, res, next) => {
     console.error(error)
   }
 })
-// charRouter.put("/clean", async(req,res,next) => {
-//   try {
-//     const newA = []
-//     for (const c of db.data.characters) {
-//       if(!newA.map(e => e.id).includes(c.id)) {
-//         newA.push(c)
-//       }
-//     }
-//     db.data.characters = newA
-//     await db.write()
-//     res.send(201)
-//   } catch (error) {
 
-//   }
-// })
-
-// charRouter.put("/location", async (req, res, next) => {
-//   try {
-//     const chars = await db.data.characters
-//     for (const char of chars) {
-//       let res = await fetch(
-//         "http://www.supernaturalwiki.com/" +
-//           char.name.replaceAll("'", "%27").replaceAll("&", "%26").replaceAll()
-//       )
-//       let txt = await res.text()
-//       const parsed = new DomParser().parseFromString(txt)
-//       let location = parsed.getElementsByTagName("table")[0]?.getElementsByTagName("td")[
-//         parsed
-//           .getElementsByTagName("td")
-//           .findIndex((el) => el?.innerHTML.includes("Location")) + 1
-//       ]?.innerHTML
-//       if(location?.includes("<a")) {
-//         location = parsed.getElementsByTagName("table")[0].getElementsByTagName("td")[
-//           parsed
-//             .getElementsByTagName("td")
-//             .findIndex((el) => el?.innerHTML.includes("Location")) + 1
-//         ].getElementsByTagName("a")[0]?.innerHTML
-//       }
-//       console.log(char.name, location)
-//       char.location = location || "Unknown"
-//     }
-//     db.data.characters = chars
-//     db.write()
-//   } catch (error) {
-//     next(error)
-//     console.log(error)
-//   }
-// })
 
 charRouter.get("/:id", async (req, res, next) => {
   try {
